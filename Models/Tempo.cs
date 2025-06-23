84a86fa0ac3cb1fd9cb659c9ad6d5c7dd56c979d
JJoaoDhann <joaomigueldhann@gmail.com>
2025-06-23 10:54:53 -0300

    Add files via upload
    correção

--- Models/Tempo.cs
+++ Models/Tempo.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MauiAppTempoAgoraSQlite;
 
-namespace MauiAppTempoSQLite.Models
+namespace MauiAppTempoAgoraSQLite.Models
 {
-    internal class Tempo
+    public class Tempo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Cidade { get; set; }
+        public DateTime DataConsulta { get; set; }
+        public double? lon { get; set; }
+        public double? lat { get; set; }
+        public double? temp_min { get; set; }
+        public double? temp_max { get; set; }
+        public int? visibility { get; set; }
+        public double? speed { get; set; }
+        public string? main { get; set; }
+        public string? description { get; set; }
+        public string? sunrise { get; set; }
+        public string? sunset { get; set; }
+    }
+
+    internal class AutoIncrementAttribute : Attribute
+    {
+    }
+
+    internal class PrimaryKeyAttribute : Attribute
     {
     }
 }
